92e55cf71b869df6a9b4dbe1ffe49fd0cc75c1d0
coder2110 <140773879+coder2110@users.noreply.github.com>
2023-12-19 16:32:57 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//Title of my game
+//The Ultimate Frog Jumper
+
+//Goal of the Game
+//Get the highest score possible
+
+//A frog is jumping in a forest. He wants to avoid hitting the rocks while also catching the most amount of mice to eat with his group
+//of friends
+
+// 1 // frog // He can jump and move left and right
+
+// 1 // mice // Made to get eaten by the frog and add points to the score
+// 2 // rocks // Obstacles that the frog has to avoid
+
+// I plan to make my game engaging by adding fun obstacle and quick jumps
